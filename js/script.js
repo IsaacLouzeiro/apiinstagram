4f270c4820c8b1ef15e43eccb1ac9788210b25fb
Isaac E. Louzeiro <isaacelias1110@gmail.com>
2021-11-16 21:32:35 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$function(){
+$(function(){
 
     const token = "IGQVJWZAWR4WVp2VC1POEp3dUNuMWtXWXVpUUNGTHFLcEhIVy01bk5TUmNMRGMtb244U3hFQmRybzNwZAU04NFRodlA4RENZAQ1Y4UnE5cEFtUjhyREJfSllWYUlMcFR1VzcwQVJ0M3FEaTN4Xy1KU1dZASgZDZD";
     const url = "https://graph.instagram.com/me/media?access_token"+token+"&fields=media_url,media_type,caption,permalink";
@@ -7,4 +7,4 @@ $function(){
         console.log('retorno: ', response.data);
     })
 
-};
+});
