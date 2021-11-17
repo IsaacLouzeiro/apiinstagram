f3a0d8c80dbb62a700b72961d8d49d5c4305a025
Isaac E. Louzeiro <isaacelias1110@gmail.com>
2021-11-16 21:41:48 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(function(){
 
     const token = "IGQVJWZAWR4WVp2VC1POEp3dUNuMWtXWXVpUUNGTHFLcEhIVy01bk5TUmNMRGMtb244U3hFQmRybzNwZAU04NFRodlA4RENZAQ1Y4UnE5cEFtUjhyREJfSllWYUlMcFR1VzcwQVJ0M3FEaTN4Xy1KU1dZASgZDZD";
-    const url = "https://graph.instagram.com/me/media?access_token"+token+"&fields=media_url,media_type,caption,permalink";
+    const url = "https://graph.instagram.com/me/media?access_token="+token+"&fields=media_url,media_type,caption,permalink";
 
     $.get(url).then(function(response){
         console.log('retorno: ', response.data);
