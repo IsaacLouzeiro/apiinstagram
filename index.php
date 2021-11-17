0a3cfb54116e860a35d8070b0a54cf6d542adc44
Isaac E. Louzeiro <isaacelias1110@gmail.com>
2021-11-16 22:04:51 -0300

    mostrando imagem do insta no site

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 
   </head>
   <body>
-      
+    <div id="insta"></div>
       
     <!-- jquery -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
--- js/script.js
+++ js/script.js
@@ -4,7 +4,27 @@ $(function(){
     const url = "https://graph.instagram.com/me/media?access_token="+token+"&fields=media_url,media_type,caption,permalink";
 
     $.get(url).then(function(response){
-        console.log('retorno: ', response.data);
+        // console.log('retorno: ', response.data);
+
+        let dadosJson = response.data
+        let conteudo = '<div class="row ps-2">';
+
+        for (let p = 0; p < dadosJson.length; p++) {
+            let feed = dadosJson[p];
+            let titulo = feed.caption !== null ? feed.caption : '';
+            let tipo = feed.media_type;
+            if(tipo === 'VIDEO') {
+                conteudo += '<div class="col-12"><video controls><source src="'+feed.media_url+'" type="video/mp4"></video></div>';
+            }
+            else if(tipo === 'IMAGE') {
+                conteudo += '<div class="col-12"><img src="'+feed.media_url+'" onclick="window.open(\''+feed.permalink+'\');" title="'+titulo+'" alt="'+titulo+'"></div>';
+            }    
+        }
+
+        conteudo += '</div>';
+
+        $('#insta').html(conteudo);
+
     })
 
 });
