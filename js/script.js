4a6ccee185186499cfdf3d3cf4f5c817d385f44e
Isaac E. Louzeiro <isaacelias1110@gmail.com>
2021-11-21 17:16:17 -0300

    colocando nome de usuario pela API

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 $(function(){
 
     const token = "IGQVJWZAWR4WVp2VC1POEp3dUNuMWtXWXVpUUNGTHFLcEhIVy01bk5TUmNMRGMtb244U3hFQmRybzNwZAU04NFRodlA4RENZAQ1Y4UnE5cEFtUjhyREJfSllWYUlMcFR1VzcwQVJ0M3FEaTN4Xy1KU1dZASgZDZD";
-    const url = "https://graph.instagram.com/me/media?access_token="+token+"&fields=media_url,media_type,caption,permalink";
+    const url = "https://graph.instagram.com/me/media?access_token="+token+"&fields=media_url,media_type,caption,permalink,username";
 
     $.get(url).then(function(response){
         // console.log('retorno: ', response.data);
 
-        let dadosJson = response.data
+        let dadosJson = response.data;
         let conteudo = '';
+        let nameUser = ''+dadosJson[0].username+'';
 
         for (let p = 0; p < dadosJson.length; p++) {
             let feed = dadosJson[p];
@@ -24,6 +25,7 @@ $(function(){
         conteudo += '</div>';
 
         $('#insta').html(conteudo);
+        $('#nameUser').html(nameUser);
 
     })
 
